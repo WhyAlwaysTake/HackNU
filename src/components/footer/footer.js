3a0d04ad13d90b82dd8dedb11019982ddd82881d
Temirlan Nurmakhan <nurmakhan.temirlan@gmail.com>
2023-04-15 21:57:54 +0600

    commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,11 +4,28 @@ const Footer = () => {
     return(
         <div className = "footer">
             <div className = "footerContainer">
-                <div className = "footer-info">
+                <div className = "footerLeft">
+                    <div className = "footerLeftLogo">
+                        <label className = "footerLeftLogoName1">e</label>
+                        <label className = "footerLeftLogoName2">Zhetkizu</label>
+                        <div className="vl">
+                        </div>
+                    </div>
+                    
+                    <div className = "footerLeftName">
+                        <label className = "footerLeftNameH">Доставка государственных</label>
+                        <label className = "footerLeftNameH">документов на дом</label>
+                    </div>
                 </div>
-                <div className = "footer-map">
+                
+                <div className = "footerRight">
+                    <div className = "footerRightMain">
+                        <label className = "footerRightL">Служба поддержки:  +7 700 000 00 00</label>
+                        <label className = "footerRightH">info@ekyzmet.kz</label>
+                        <label className = "footerRightL">2022 (C) Все права защишены</label>
+                    </div>
                 </div>
-            </div>
+            </div>  
         </div>
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,8 +6,28 @@ const Header = () => {
     return (
         <div className = "header">
             <div className = "headerContainer">
-                <div className = "headerList">
-                    <div className = "headerListItem active">
+                <div className = "headerLeft">
+                    <div className = "headerLeftLogo">
+                        <label className = "headerLeftLogoName1">e</label>
+                        <label className = "headerLeftLogoName2">Zhetkizu</label>
+                        <div className="vl">
+                        </div>
+                    </div>
+                    
+                    <div className = "headerLeftName">
+                        <label className = "headerLeftNameH">Доставка государственных</label>
+                        <label className = "headerLeftNameH">документов на дом</label>
+                    </div>
+                </div>
+
+                <div className = "headerRight">
+                    <div className = "headerRightLan">
+                        <label className = "headerRightLanL">ҚАЗ</label>
+                        <label className = "headerRightLanL">РУС</label>
+                    </div>
+                    <div className = "headerRightLogin">
+                        <logo className = "headerRightLoginLogo"></logo>
+                        <label className = "headerRightLoginL">Вход</label>
                     </div>
                 </div>
             </div>  
